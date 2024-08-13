e9f223017ad9a389116d49f641fc9e2303aff81c
PrasanthChandaka <prasanthchandaka111a@gmail.com>
2024-08-13 23:05:35 +0530

    v

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/layout";
 import toast from "react-hot-toast";
 import { MdDelete, MdEdit } from "react-icons/md";
